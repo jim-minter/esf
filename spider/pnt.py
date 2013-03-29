950495c7d25151f67b4c4b4f5705882d8c40f28c
Jim Minter <jminter@redhat.com>
2013-03-29 10:30:32 +0000

    ignore archive

---
+++ spider/pnt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+
+import config
+import spider
+
+class PntRepo(spider.Repo):
+  name = "pnt"
+
+  def walk(self):
+    base = config.get("pnt-filebase")
+    for dirpath, dirnames, filenames in os.walk(base):
+      dirnames[:] = sorted(dirnames)
+
+      for f in sorted(filenames):
+        p = os.path.join(dirpath, f)
+        url = os.path.join(config.get("pnt-urlbase"),
+                           os.path.relpath(p, base))
+
+        yield PntDocument(self, f, url, p)
+
+
+class PntDocument(spider.LocalDocument):
+  def __init__(self, repo, name, url, basepath, ancestors = []):
+    super(spider.LocalDocument, self).__init__(repo, name, url, ancestors)
+    self.basepath = basepath
+
+
+def main():
+  spider.Spider(PntRepo()).index()
+
+if __name__ == "__main__":
+  main()
